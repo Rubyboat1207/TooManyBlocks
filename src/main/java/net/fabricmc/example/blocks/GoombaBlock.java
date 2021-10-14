24bb06126c25585ff17fe89d53b5e95c68215253
Rubyboat1207 <rudy@rudysoliz.com>
2021-10-14 15:40:40 -0500

    Added Goomba Block (No Function)

---
+++ src/main/java/net/fabricmc/example/blocks/GoombaBlock.java
@@ -0,0 +1,9 @@
+package net.fabricmc.example.blocks;
+
+import net.minecraft.block.Block;
+
+public class GoombaBlock extends Block {
+    public GoombaBlock(Settings settings) {
+        super(settings);
+    }
+}
